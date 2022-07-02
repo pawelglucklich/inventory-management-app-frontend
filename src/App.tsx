89f1c1b5c39afd0aa15a3d33c43d10c0c0e3ba20
pawelglucklich <pawel.glucklich@gmail.com>
2022-07-02 14:21:39 +0200

    hotfix: config-overrides added

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+
+export const App = () => {
+
+    return (
+      <Routes>
+          <Route path="/" element={''}/>
+      </Routes>
+    );
+}
