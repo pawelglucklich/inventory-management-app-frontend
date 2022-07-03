a3e994d048ff4ce7ba2474792607b4bf8f1a178d
pawelglucklich <pawel.glucklich@gmail.com>
2022-07-03 17:36:49 +0200

    started building frontend main app window

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
+import { Footer } from './components/Footer/Footer';
+import {Header} from "./components/layout/Header";
+import {MainApp} from "./components/MainApp/MainApp";
 
 export const App = () => {
 
     return (
-      <Routes>
-          <Route path="/" element={''}/>
-      </Routes>
+        <>
+            <Header></Header>
+            <MainApp></MainApp>
+            {/*<Routes>*/}
+            {/*    <Route path="/" element={''}/>*/}
+            {/*</Routes>*/}
+            <Footer></Footer>
+        </>
     );
 }
