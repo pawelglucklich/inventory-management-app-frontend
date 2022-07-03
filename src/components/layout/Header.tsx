62c628a4a0e0f15638d15bb33320a1c699ea292d
pawelglucklich <pawel.glucklich@gmail.com>
2022-07-03 17:35:22 +0200

    added new component - header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Header.css';
+
+export const Header = () => {
+    return (
+        <header>
+            <img src="..." alt="Company logo" className="company-logo"/>
+            <h1 className="company-name">Company Name</h1>
+        </header>
+    );
+};
