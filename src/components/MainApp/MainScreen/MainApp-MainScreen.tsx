50bb1497f8d64f7686f5b00cc06d2517994825cd
pawelglucklich <pawel.glucklich@gmail.com>
2022-07-06 21:46:51 +0200

    MainApp-MainScreen added, and styling.

---
+++ src/components/MainApp/MainScreen/MainApp-MainScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './MainApp-MainScreen.css';
+
+export const MainAppMainScreen = () => {
+    return (
+        <div className={'main-app-main-screen'}/>
+    );
+};
