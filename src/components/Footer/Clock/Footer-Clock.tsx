399f55ed56b64b01a6ac28e9cccdd8487a3a5633
pawelglucklich <pawel.glucklich@gmail.com>
2022-07-06 21:44:19 +0200

    Footer module - added style. Footer-Clock - added clock module to footer.

---
+++ src/components/Footer/Clock/Footer-Clock.tsx
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react'
+
+export const FooterClock = () => {
+
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => setDate(new Date()), 1000)
+        return function cleanup() {
+            clearInterval(timer)
+        };
+
+    });
+
+    return <p>
+        {date.toLocaleTimeString('default', {
+            hourCycle: 'h24'
+        })}
+    </p>
+};
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Btn } from "../common/Btn";
+import {Btn} from "../common/Btn/Btn";
 
 import './Footer.css';
+import {FooterClock} from "./Clock/Footer-Clock";
 
 export const Footer = () => {
     return (
         <footer>
             <Btn text={'Men1'}/>
             <Btn text={'Men2'}/>
-            <h2>John Smith</h2>
-            <p>00:00</p>
+            <div className={'footer-right'}>
+                <h2>John Smith</h2>
+                <FooterClock/>
+            </div>
         </footer>
     );
 };
