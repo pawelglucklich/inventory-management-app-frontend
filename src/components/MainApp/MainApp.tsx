e97800c536f5e10a93ce6b4f63fce62161db9296
pawelglucklich <pawel.glucklich@gmail.com>
2022-07-06 21:47:51 +0200

    Added left, right and main-screen panels to MainApp.

--- src/components/MainApp/MainApp.tsx
+++ src/components/MainApp/MainApp.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 import './MainApp.css';
+import {MainAppLeftPanel} from "./LeftPanel/MainApp-LeftPanel";
+import {MainAppMainScreen} from "./MainScreen/MainApp-MainScreen";
+import {MainAppRightPanel} from "./RightPanel/MainApp-RightPanel";
 
 export const MainApp = () => {
-    return <div className={'main-app'}/>;
+    return (
+        <div className={'main-app'}>
+            <MainAppLeftPanel/>
+            <MainAppMainScreen/>
+            <MainAppRightPanel/>
+        </div>
+    );
 };
