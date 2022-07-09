578075e5d0402b8b1d7be5c1dd2f4151edaff16e
pawelglucklich <pawel.glucklich@gmail.com>
2022-07-09 23:36:15 +0200

    css

--- src/components/MainAppLocation/Location-MainScreen/LocationMainScreen.tsx
+++ src/components/MainAppLocation/Location-MainScreen/LocationMainScreen.tsx
@@ -1,17 +1,19 @@
 import React from "react";
+import {useGlobalState} from "../../utils/state";
 
 import './LocationMainScreen.css';
-import {useGlobalState} from "../../utils/state";
 
 export const LocationMainScreen = () => {
     const [val, setVal] = useGlobalState('screenValue')
 
-    const valScreen = val[4] ? `${val[0]} ${val[1]} . ${val[2]} ${val[3]} . ${val[4]}`
-        : val[3] ? `${val[0]} ${val[1]} . ${val[2]} ${val[3]} . _`
-            : val[2] ? `${val[0]} ${val[1]} . ${val[2]} _ . _`
-                : val[1] ? `${val[0]} ${val[1]} . _ _ . _`
-                    : val[0] ? `${val[0]} _ . _ _ . _`
-                        : `_ _ . _ _ . _`
+    const valScreen = (val[4] ? `${val[0]} ${val[1]} . ${val[2]} ${val[3]} . ${val[4]}`
+            : val[3] ? `${val[0]} ${val[1]} . ${val[2]} ${val[3]} . _`
+                : val[2] ? `${val[0]} ${val[1]} . ${val[2]} _ . _`
+                    : val[1] ? `${val[0]} ${val[1]} . _ _ . _`
+                        : val[0] ? `${val[0]} _ . _ _ . _`
+                            : `_ _ . _ _ . _`
+    );
+
 
     return (
         <div className={'location-main-screen'}>
